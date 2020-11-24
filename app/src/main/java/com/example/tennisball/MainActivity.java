4d16db037780f51123979ad8795173d755be9815
Partast1 <andersa1989@gmail.com>
2020-11-24 01:00:43 +0100

    Initial commit

---
+++ app/src/main/java/com/example/tennisball/MainActivity.java
@@ -0,0 +1,125 @@
+package com.example.tennisball;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorEventListener2;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener2 {
+
+    private float xPos, xAccel, xVel = 0.0f;
+    private float yPos, yAccel, yVel = 0.0f;
+    private float xMax, yMax;
+    private Bitmap ball;
+    private SensorManager sensorManager;
+
+    /*
+    * Private class ballview bliver sat som contentView
+    * */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        BallView ballView = new BallView(this);
+        setContentView(ballView);
+
+        Point size = new Point();
+        Display display = getWindowManager().getDefaultDisplay();
+        display.getSize(size);
+        xMax = (float) size.x - 100;
+        yMax = (float) size.y - 100;
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+    }
+    //registrer sensormanager og indstiller delay
+    @Override
+    protected void onStart() {
+        super.onStart();
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_GAME);
+    }
+    //afregistrer sensormanager
+    @Override
+    protected void onStop() {
+        sensorManager.unregisterListener((SensorEventListener) this);
+        super.onStop();
+    }
+    //Lytter på ændringer på boldens position.
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            xAccel = sensorEvent.values[0];
+            yAccel = -sensorEvent.values[1];
+            updateBall();
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    //opdatere positionen af bolden
+    private void updateBall() {
+        float frameTime = 0.666f;
+        xVel += (xAccel * frameTime);
+        yVel += (yAccel * frameTime);
+
+        float xS = (xVel / 2) * frameTime;
+        float yS = (yVel / 2) * frameTime;
+
+        xPos -= xS;
+        yPos -= yS;
+
+        if (xPos > xMax) {
+            xPos = xMax;
+        } else if (xPos < 0) {
+            xPos = 0;
+        }
+
+        if (yPos > yMax) {
+            yPos = yMax;
+        } else if (yPos < 0) {
+            yPos = 0;
+        }
+    }
+
+    @Override
+    public void onFlushCompleted(Sensor sensor) {
+
+    }
+
+    private class BallView extends View {
+
+        public BallView(Context context) {
+            super(context);
+            Bitmap ballSrc = BitmapFactory.decodeResource(getResources(), R.drawable.ball);
+            final int dstWidth = 100;
+            final int dstHeight = 100;
+            ball = Bitmap.createScaledBitmap(ballSrc, dstWidth, dstHeight, true);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            canvas.drawBitmap(ball, xPos, yPos, null);
+            invalidate();
+        }
+    }
+}
